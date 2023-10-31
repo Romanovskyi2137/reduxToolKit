05567f8ce4b76e0857324fdee4e5eaa790552bf1
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2023-10-31 16:29:22 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  let counter = 0;
+  return (
+    <div className="App">
+      <h1>{counter}</h1>
+      <button>increment</button>
+      <button>decrement</button>
+    </div>
+  );
+}
+
+export default App;
